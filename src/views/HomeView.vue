b7b253747be0ed4193c4e2ab19447f8e08ba6740
Benedict Umeozor <benedictumeozor@gmail.com>
2025-10-15 07:31:11 +0100

    Fetch specific fields from countries API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@ import { ref, computed } from 'vue'
 const { data, isPending, isError } = useQuery({
   queryKey: ['countries'],
   queryFn: async (): Promise<CountryInterface[]> => {
-    const response = await fetch('https://restcountries.com/v3.1/all')
+    const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca3')
     return response.json()
   },
   staleTime: Infinity,
